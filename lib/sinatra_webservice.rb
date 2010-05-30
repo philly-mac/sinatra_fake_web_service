2ae9ec132ea9f92d66c8a6fcae9c42eaa2f13c8c
Philip MacIver <philip@ivercore.com>
2010-05-30 22:37:09 +0200

    Moved the sleep call outside of the spawned thread

--- lib/sinatra_webservice.rb
+++ lib/sinatra_webservice.rb
@@ -2,29 +2,29 @@ require 'net/http'
 require 'uri'
 
 class SinatraWebService
-  
+
   attr_accessor :host, :port
   attr_accessor :current_thread
-  
-  class SinatraStem < Sinatra::Base      
-    
+
+  class SinatraStem < Sinatra::Base
+
     enable :methodoverride
-    
+
     get '/' do
       "Hello! i am lindesy lohan!"
     end
-    
+
     post '/gimmie' do
       "here ya go"
     end
   end
-  
-  
+
+
   def initialize(options = {})
     @host = options[:host] ||= 'localhost'
     @port = options[:port] ||= 4567
   end
-  
+
   def running?
     self.current_thread.alive? rescue false
   end
@@ -33,15 +33,15 @@ class SinatraWebService
     if Thread.list.size > 2
       Thread.list.first.kill
     end
-    
+
     @port = find_free_port
-    
+
     self.current_thread = Thread.new do
-        SinatraStem.run! :post => @host, :port => @port.to_i
-      sleep 1
+      SinatraStem.run! :post => @host, :port => @port.to_i
     end
+    sleep 2
   end
-  
+
   def find_free_port
     found = false
     attempts = 0
@@ -58,33 +58,34 @@ class SinatraWebService
       end
     end
   end
-  
+
   def get_response(action)
     res = Net::HTTP.start(self.host, self.port) do |http|
       http.get(action)
     end
   end
-  
+
   def delete_response(action, data = "", headers = nil, dest = nil)
     data = data.empty? ? "_method=delete" : data += "&_method=delete"
     post_response(action, data, headers, dest)
   end
-  
+
   def put_response(action, data = "", headers = nil, dest = nil)
     data = data.empty? ? "_method=put" : data += "&_method=put"
     post_response(action, data, headers, dest)
   end
-  
+
   def post_response(action, data, headers = nil, dest = nil)
     res = Net::HTTP.start(self.host, self.port) do |http|
       http.post(action, data, headers, dest)
     end
   end
-  
+
   def method_missing(method, *args, &block)
     SinatraStem.instance_eval do |base|
       route method.to_s.upcase, *args, &block
-    end 
+    end
   end
 
 end
+
